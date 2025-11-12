055076561681752077b3c083e48e6ccb0c7b0d90
Himanshu Saxena <himanshu@ogrelogic.com>
2025-11-12 15:16:03 +0530

    code seo tag updatd

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'RadioGoldInformation – Historic Radio Broadcasts & Broadcasters';
+export const SITE_DESCRIPTION = 'RadioGoldInformation.com is your gateway to historic radio broadcasts and broadcasters. Explore indexes, archives and more.';
 export const BASE_URL = "https://calm-appliance-da5b13442b.strapiapp.com"
